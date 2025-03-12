7e010ebc79fcacf9b7b8cad239367515ec010b6d
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-03-12 11:34:36 +0200

    About section expanded

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,6 @@
 <script></script>
 <template>
-  <div
-    class="flex flex-col xl:flex-row bg-black justify-center items-center pt-8 pb-14"
-  >
+  <div class="flex flex-col xl:flex-row bg-black justify-center items-center pt-16 pb-10">
     <div class="flex flex-col justify-start text-white xl:order-last">
       <p class="text-3xl xl:text-5xl text-amber-100 font-bold mb-6 xl:mb-14">
         Szólj ha segíthetünk
@@ -12,40 +10,18 @@
         <p class="underline">+40 (735) 500 956</p>
       </div>
       <p class="flex text-xl xl:text-2xl font-bold">varadifi@gmail.com</p>
-      <div
-        class="flex my-2 xl:my-10 justify-center items-center xl:justify-start"
-      >
-        <a
-          href="https://www.instagram.com/varadifi/"
-          target="_blank"
-          class="text-white"
-        >
-          <i
-            class="bi bi-instagram text-3xl bg-gray-800 px-3 pt-1 pb-3 rounded-full me-4"
-          ></i>
+      <div class="flex my-2 xl:my-10 justify-center items-center xl:justify-start">
+        <a href="https://www.instagram.com/varadifi/" target="_blank" class="text-white">
+          <i class="bi bi-instagram text-3xl bg-gray-800 px-3 pt-1 pb-3 rounded-full me-4"></i>
         </a>
-        <a
-          href="https://www.facebook.com/varadifi"
-          target="_blank"
-          class="text-white"
-        >
-          <i
-            class="bi bi-facebook text-3xl bg-gray-800 px-3 pt-1 pb-3 rounded-full me-4"
-          ></i>
+        <a href="https://www.facebook.com/varadifi" target="_blank" class="text-white">
+          <i class="bi bi-facebook text-3xl bg-gray-800 px-3 pt-1 pb-3 rounded-full me-4"></i>
         </a>
-        <a
-          href="https://www.youtube.com/@varadifi"
-          target="_blank"
-          class="text-white"
-        >
-          <i
-            class="bi bi-youtube text-3xl bg-gray-800 px-3 pt-1 pb-3 rounded-full me-4"
-          ></i>
+        <a href="https://www.youtube.com/@varadifi" target="_blank" class="text-white">
+          <i class="bi bi-youtube text-3xl bg-gray-800 px-3 pt-1 pb-3 rounded-full me-4"></i>
         </a>
       </div>
-      <p class="flex text-xl xl:text-2xl font-bold underline mb-2">
-        Gyülekezeti alkalmaink:
-      </p>
+      <p class="flex text-xl xl:text-2xl font-bold underline mb-2">Gyülekezeti alkalmaink:</p>
       <p class="flex text-xl xl:text-2xl font-bold mb-2">Csütörtök 18:00</p>
       <p class="flex text-xl xl:text-2xl font-bold">Vasárnap 10:00 és 18:00</p>
     </div>
@@ -56,7 +32,25 @@
     />
   </div>
   <div class="flex bg-black justify-center items-center">
-    <p class="text-gray-700 text-xs pb-5">Made by SzBRoli</p>
+    <p
+      class="flex text-white font-bold text-xl xl:text-2xl tracking-widest pb-10 px-64 text-center"
+    >
+      Ha keresed a helyedet, ha keresed, hogy ki vagy, ha keresel egy helyet, ahol önmagad lehetsz,
+      ha keresel valakit, akit barátnak nevezz, ha keresed Istent… ITT A HELYED! Ha Nagyváradon
+      tanulsz és szeretnél egy befogadó közösség része lenni, akkor várunk a Váradifibe. Minden
+      héten szerdán 7 órától találkozunk a Berthelot utca 5. szám alatt, és szeretnénk, ha te is
+      részese lennél alkalmainknak. Adj egy lehetőséget nekünk, nem bánod meg!
+    </p>
+  </div>
+  <div class="flex bg-black justify-center items-center">
+    <p class="text-gray-400 text-lg pb-10 px-64 text-center">
+      Nagyváradi keresztény ifjúsági csoport, akik heti szinten találkozunk, hogy beszélgessünk,
+      dicsőitsünk és tanuljunk a Biblából. Várunk téged is minden szerdán 7től!
+    </p>
+  </div>
+
+  <div class="flex bg-black justify-center items-center">
+    <p class="text-gray-700 text-xs pb-10">Made by SzBRoli</p>
   </div>
 </template>
 <style>
